8fc6beae482be062e2d0987ba142e92b6b32e0d2
Ramiro440 <ramirohaces44@gmail.com>
2023-06-12 14:54:25 -0500

    Se genera page
    Se crea page en index.js, se agrega styles y se genera character como inicio.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
+function getPage() {
+    const url = window.location.search
+    const urlParams = new URLSearchParams(url)
+    const page = urlParams.get('page') || 1
+    return page
+}
+
 async function fetching () {
-    const response = await fetch("https://rickandmortyapi.com/api/character")
+    const page = getPage()
+    const endpoint = `https://rickandmortyapi.com/api/character?page=${page}`
+
+    const response = await fetch(endpoint)
     const json = await response.json()
     return json
 }
 
 fetching().then(
     response => {
+        paginationWrapper(response.info)
         let template = ``
         response.results.forEach(item => {
             template += `
@@ -24,3 +35,17 @@ fetching().then(
         results.innerHTML = template
     }
 )
+
+function paginationWrapper (info) {
+    const actualPage = getPage()
+    let template = ``
+    for (let index = 1; index <= info.pages; index++) {
+        template +=`
+        <div class="page ${actualPage == index ? 'current': ''}">
+        <a href="index.html?page=${index}">${index}</a>
+        </div>
+        `
+        
+    }
+    pagination.innerHTML = template
+}
